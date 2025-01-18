5dd5dfc94e4c2e1de0cc5b237498293d44bdcb01
OzyNode <oguzhandbrl@gmail.com>
2025-01-18 18:13:51 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
         <Button href="https://x.com/OzzyyDebreli" prefixIcon="twitter" size="l" variant="tertiary">
           Twitter
         </Button>
-        <Button href="https://t.me/OzyNode" prefixIcon="telegram" size="l" variant="tertiary">
+        <Button href="https://www.t.me/OzyNode" prefixIcon="telegram" size="l" variant="tertiary">
           Telegram
         </Button>
       </Flex>
